8ca0ff4382d0e5fbda072885d2785b11f78a960a
mehedinaeem <mehedinaeem00@gmail.com>
2023-12-14 11:08:29 +0600

    js

---
+++ 7.Software_Devolopment/Front_end/week-02/module-05/3,operation.js
@@ -0,0 +1,16 @@
+console.log("Do here some operation")
+
+num1=100.78;
+num2=70;
+
+result=num1+num2;
+console.log(result);
+console.log(parseInt(result))
+console.log(parseFloat(result).toFixed(4))
+
+num3="100"
+num4=20
+r=num3+num4
+console.log(r)
+console.log(num3*num4)
+console.log(typeof(r))
