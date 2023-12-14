37c596e7ea895ec34e05708e945ba870f2d09210
mehedinaeem <mehedinaeem00@gmail.com>
2023-12-14 13:53:37 +0600

    js

---
+++ 7.Software_Devolopment/Front_end/week-02/module-05/4.condition.js
@@ -0,0 +1,11 @@
+console.log("Here done work with condition")
+tk = 1000
+if (tk < 100) {
+    console.log("ruti khamu")
+}
+else if (tk == 100) {
+    console.log("Biriyani khamu")
+}
+else {
+    console.log("teheri khamu")
+}
