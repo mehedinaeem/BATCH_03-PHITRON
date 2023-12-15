8b1f98c35dc3b0d2f8122b3da4c983068c4b2b18
mehedinaeem <mehedinaeem00@gmail.com>
2023-12-15 09:49:45 +0600

    js

--- 7.Software_Devolopment/Front_end/week-02/module-06/1.app.js
+++ 7.Software_Devolopment/Front_end/week-02/module-06/1.app.js
@@ -12,18 +12,50 @@
 // // remove color
 // document.getElementById("check").style.removeProperty("color");
 
-var img=document.getElementById("img");
-console.log(img.getAttribute("src"));
 
-// set attribute
-img.setAttribute("alt","picture");
-console.log(img.getAttribute("alt"));
+// module 6.3
+// var img=document.getElementById("img");
+// console.log(img.getAttribute("src"));
 
-var hero=document.getElementById("hero");
-console.log(hero.innerText);
+// // set attribute
+// img.setAttribute("alt","picture");
+// console.log(img.getAttribute("alt"));
 
-var input=document.getElementById("input").value;
-console.log(input);
+// var hero=document.getElementById("hero");
+// console.log(hero.innerText);
 
-var parent=document.getElementById("parent").innerHTML;
-console.log(parent);
+// var input=document.getElementById("input").value;
+// console.log(input);
+
+// var parent=document.getElementById("parent").innerHTML;
+// console.log(parent);
+
+
+// module 6.4
+var testDiv = document.getElementsByClassName("right");
+console.log(testDiv[0].childNodes[1].parentNode.parentNode.parentNode.childNodes[5]);
+
+
+// append
+var newDiv = document.getElementById("newDiv");
+var p = document.createElement("p");
+p.innerText = "Succesfully i created that";
+newDiv.append(p);
+
+// do it by function
+function append_maker() {
+    var newDiv = document.getElementById("newDiv");
+    var p = document.createElement("p");
+    p.innerText = "Succesfully i created that by function";
+    newDiv.append(p);
+}
+append_maker();
+append_maker();
+append_maker();
+append_maker();
+
+// module 6.5 create event,button
+document.getElementById("submit-btn").addEventListener("click", function (e) {
+    console.log("yes i created the button");
+    append_maker();
+});
