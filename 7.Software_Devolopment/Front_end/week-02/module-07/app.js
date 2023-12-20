233dc410a0d3f654ec52454ccff6d360be864a59
mehedinaeem <mehedinaeem00@gmail.com>
2023-12-20 06:33:58 +0600

    js

--- 7.Software_Devolopment/Front_end/week-02/module-07/app.js
+++ 7.Software_Devolopment/Front_end/week-02/module-07/app.js
@@ -8,21 +8,43 @@
 //     console.log(arr);
 // });
 
-console.log("1");
-console.log("2");
-console.log("3");
-console.log("4");
-show(5);
-console.log("6");
-console.log("7");
-console.log("8");
-console.log("9");
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// show(5);
+// console.log("6");
+// console.log("7");
+// console.log("8");
+// console.log("9");
 
-function show(value) {
-    // console.log(value);
-    fetch("https://fakestoreapi.com/products")
-        .then(res => res.json())
-        .then((data) => {
-            console.log(data);
-        })
+// function show(value) {
+//     // console.log(value);
+//     fetch("https://fakestoreapi.com/products")
+//         .then(res => res.json())
+//         .then((data) => {
+//             console.log(data);
+//         })
+// }
+
+// const getData=new Promise((resolve, reject) => {
+//     return resolve(20);
+// });
+// getData.then(data=>console.log(data));
+
+
+// const getData=new Promise((resolve, reject) => {
+//     return reject(20);
+// });
+// console.log(getData);
+
+
+
+// asycronus
+const loadData=async()=>{
+    const responce=await fetch("https://fakestoreapi.com/products");
+    const data=await responce.json();
+    console.log(data);
 }
+
+loadData();
