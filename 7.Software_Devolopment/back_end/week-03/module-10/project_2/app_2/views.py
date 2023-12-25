e503b86c533e932384d5cc72eaf20c3eedabc42a
mehedinaeem <mehedinaeem00@gmail.com>
2023-12-25 11:17:29 +0600

    render from app

--- 7.Software_Devolopment/back_end/week-03/module-10/project_2/app_2/views.py
+++ 7.Software_Devolopment/back_end/week-03/module-10/project_2/app_2/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("This is homepage")
+    # return HttpResponse("This is homepage")
+    # return render('')
+    return render(request,"app_2/index.html")
