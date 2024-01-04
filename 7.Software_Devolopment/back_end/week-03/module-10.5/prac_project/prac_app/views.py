f6291e8ce933549ac4026232f5341262fa364669
mehedinaeem <mehedinaeem00@gmail.com>
2024-01-04 10:09:08 +0600

    before url module 10.5 complete

--- 7.Software_Devolopment/back_end/week-03/module-10.5/prac_project/prac_app/views.py
+++ 7.Software_Devolopment/back_end/week-03/module-10.5/prac_project/prac_app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
+
+
 def home(request):
-    context={
-        'Name':'Naeem',
-        'Age':20,
-        'Dept':'CSE',
-        'Session':'21-22',
+    context = {
+        'Name': 'naeem',
+        'Age': 20,
+        'Dept': 'CSE',
+        'Session': '21-22',
+        'date': datetime.now().date(),
     }
-    return render(request,'home.html',context)
+    return render(request, 'home.html', context)
